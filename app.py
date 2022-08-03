50d7d751b2aadf7cc29804b09391967da07766b9
jammi shah <jammishah@gmail.com>
2022-08-03 15:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route("/")
-### message on asking the question. ###
 def main():
-### default message on homepage. ###
+    # default message on homepage.
     return "Welcome!"
-@app.route('/how are you')
-### message on asking the question. ###
+
+
+@app.route("/how are you")
+# message on asking the question.
 def hello():
-### message on asking the question. ###
-    return 'I am good, how about you?'
+    # message on asking the question.
+    return "I am good, how about you?"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
