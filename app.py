edc90ceba9c63ec70971a940e45094e5d1905766
jammi shah <jammishah@gmail.com>
2022-08-03 12:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
+### message on asking the question. ###
 def main():
 ### default message on homepage. ###
     return "Welcome!"
 @app.route('/how are you')
+### message on asking the question. ###
 def hello():
 ### message on asking the question. ###
     return 'I am good, how about you?'
