9f670f6af799abb87f90113ca0dde8998d44e513
jammi shah <jammishah@gmail.com>
2022-08-03 12:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-###
-This code represents a small flask app that picks up questions asked through query parameters then answers them 
-Questions that can be asked are: 1. how are you
-###
+# This code represents a small flask app that picks up questions asked through query parameters then answers them 
+# Questions that can be asked are: 1. how are you
+
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-###
-default message on homepage
-###
+# default message on homepage
     return "Welcome!"
 
 @app.route('/how are you')
 def hello():
-###
-message on asking the question
-###
+# message on asking the question
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
